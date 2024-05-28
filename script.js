cb90859e8410b3654a093293fb174d3a6dd8435d
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-05-28 19:35:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 const circle = document.querySelectorAll(".circle");
-const nxtBtn = document.querySelector("#nxt");
-const prvBtn = document.querySelector("#pre");
+const nxtBtn = document.querySelector("#next");
+const prvBtn = document.querySelector("#prev");
 
 nxtBtn.addEventListener("click",addclass);
 
