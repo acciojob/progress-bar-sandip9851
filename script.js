aa75448c152ecb3e6fd12614d9e64eeba983e1d0
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-05-28 19:19:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,11 +7,19 @@ nxtBtn.addEventListener("click",addclass);
 
 let index = 0;
 function addclass() {
+	nxtBtn.preventDefault();
 	circle[index].className = "active";
 	index++;
+	if(index<0){
+		index = 0;
+		}
 	
 }
 prvBtn.addEventListener("click",removeClass);
 function removeClass() {
-	circle[--index].className = "circle";
+	prvBtn.preventDefault();
+	circle[--index].className = "circle;
+		if(index<0){
+		index = 0;
+		}
 }
