d1fa75925907527430782db972cd6205d05d2ad2
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-05-28 19:11:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const circle = document.querySelectorAll(".circle");
+const nxtBtn = document.querySelector("#nxt");
+const prvBtn = document.querySelector("#pre");
+
+nxtBtn.addEventListener("click",addclass);
+
+let index = 0;
+function addclass() {
+	circle[index].className = "active";
+	index++;
+	
+}
+prvBtn.addEventListener("click",removeClass);
+function removeClass() {
+	circle[--index].className = "circle";
+}
