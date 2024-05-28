b87b9ea1777f2eb13f8c3075c79eb7aff6ba96de
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-05-28 19:28:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,19 +7,31 @@ nxtBtn.addEventListener("click",addclass);
 
 let index = 0;
 function addclass() {
-	nxtBtn.preventDefault();
-	circle[index].className = "active";
-	index++;
-	if(index<0){
-		index = 0;
-		}
+	//nxtBtn.preventDefault();
+	if (index < circle.length) {
+    circle[index].classList.add("active");
+    index++;
+  }
+	if (index === circle.length) {
+    nxtBtn.disabled = true;
+  }
+	if (index > 0) {
+    prvBtn.disabled = false;
+  }
 	
 }
 prvBtn.addEventListener("click",removeClass);
 function removeClass() {
-	prvBtn.preventDefault();
-	circle[--index].className = "circle;
-		if(index<0){
-		index = 0;
-		}
+	//prvBtn.preventDefault();
+	if (index > 0) {
+    index--;
+    circle[index].classList.remove("active");
+  }
+	if (index === 0) {
+    prvBtn.disabled = true;
+  }
+	if (index < circle.length) {
+    nxtBtn.disabled = false;
+  }
+	
 }
